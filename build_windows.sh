e727c98b8520e42ec092e309e561a4526fba040d
Adria <adriabama06@gmail.com>
2025-08-24 17:31:02 +0000

    Yay, builds and runs on windows

--- build_windows.sh
+++ build_windows.sh
@@ -2,7 +2,18 @@
 
 # Configure with CMake
 cmake -B build-windows \
-  -DCMAKE_TOOLCHAIN_FILE=toolchain-windows.cmake
+  -DCMAKE_TOOLCHAIN_FILE=toolchain-windows.cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_CURL_EXE=OFF \
+  -DCURL_USE_OPENSSL=OFF \
+  -DCURL_USE_SCHANNEL=ON \
+  -DENABLE_TEST=OFF \
+  -DENABLE_INSTALL=OFF \
+  -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" \
+  -DCMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++"
 
-# Build with all cores
-cmake --build build-windows -j
+JOBS=$(nproc)
+
+# Build with limited threads to avoid memory issues
+cmake --build build-windows -j $((JOBS>8 ? 8 : JOBS))
--- toolchain-windows.cmake
+++ toolchain-windows.cmake
@@ -3,9 +3,14 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # Compiler settings
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# Static linking flags
+set(CMAKE_C_FLAGS_INIT "-static")
+set(CMAKE_CXX_FLAGS_INIT "-static")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static -static-libgcc -static-libstdc++")
 
 # Linker paths
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
