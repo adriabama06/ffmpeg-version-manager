7c69c2b70300c376880d72b1102bde789125ace4
adriabama06 <adriabama06@gmail.com>
2026-05-13 18:12:59 +0200

    Todo split win/linux implementation to setup env

---
+++ src/env_settings.linux.cc
@@ -0,0 +1,10 @@
+// TODO: Use #ifdef to know if the compiled code must use windows or linux
+/**
+#if linux
+
+#include "env.hh"
+
+... implement ...
+
+#endif
+ */
---
+++ src/env_settings.windows.cc
@@ -0,0 +1,10 @@
+// TODO: Use #ifdef to know if the compiled code must use windows or linux
+/**
+#if windows
+
+#include "env.hh"
+
+... implement ...
+
+#endif
+ */
