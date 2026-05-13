46f16626e9502c5eac79c6148317dccb1267802e
adriabama06 <adriabama06@gmail.com>
2026-05-13 22:54:53 +0200

    Refactor Windows path management by introducing registry helper functions

--- src/env_settings.windows.cc
+++ src/env_settings.windows.cc
@@ -15,92 +15,117 @@ namespace fs = std::filesystem;
 
 #include <windows.h>
 
-int update_windows_path(const fs::path& ffmpeg_vm_dir, bool add) {
+static std::string get_registry_value(HKEY root_key, const std::string& sub_key, const std::string& value_name)
+{
     HKEY hKey;
-    LONG lResult;
+    std::string result;
+    LONG lResult = RegOpenKeyExA(root_key, sub_key.c_str(), 0, KEY_READ, &hKey);
+    if (lResult != ERROR_SUCCESS) return result;
+
     DWORD dwType = REG_EXPAND_SZ;
     DWORD dwSize = 0;
-    std::string currentPath;
-
-    // Open environment key
-    lResult = RegOpenKeyExA(HKEY_CURRENT_USER, "Environment", 0, KEY_READ | KEY_WRITE, &hKey);
-    if (lResult != ERROR_SUCCESS) {
-        std::cerr << "Error: Could not open environment registry key" << std::endl;
-        return 1;
-    }
-
-    // Get current PATH value
-    lResult = RegQueryValueExA(hKey, "Path", NULL, &dwType, NULL, &dwSize);
-    if (lResult == ERROR_SUCCESS) {
+    lResult = RegQueryValueExA(hKey, value_name.c_str(), NULL, &dwType, NULL, &dwSize);
+    if (lResult == ERROR_SUCCESS && dwSize > 0) {
         std::vector<char> buffer(dwSize);
-        lResult = RegQueryValueExA(hKey, "Path", NULL, &dwType, (LPBYTE)buffer.data(), &dwSize);
+        lResult = RegQueryValueExA(hKey, value_name.c_str(), NULL, &dwType, (LPBYTE)buffer.data(), &dwSize);
         if (lResult == ERROR_SUCCESS) {
-            currentPath = std::string(buffer.data(), dwSize - 1); // Remove null terminator
+            result = std::string(buffer.data(), dwSize - 1);
         }
-    }
-
-    // Modify PATH
-    std::string newPath;
-    std::string dir_str = (ffmpeg_vm_dir / "bin").string();
-
-    if (add) {
-        lResult = RegSetValueExA(hKey, "FFMPEGVM_PATH", 0, REG_EXPAND_SZ, 
-                            (const BYTE*)ffmpeg_vm_dir.string().c_str(), ffmpeg_vm_dir.string().length() + 1);
-        if(lResult == ERROR_SUCCESS)
-        {
-            SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 
-                       (LPARAM)"Environment", SMTO_ABORTIFHUNG, 5000, NULL);
-        }
-
-        if (currentPath.find(dir_str) != std::string::npos) {
-            std::cout << "ffmpeg-vm already in PATH" << std::endl;
-            RegCloseKey(hKey);
-            return 0;
-        }
-        newPath = dir_str + ";" + currentPath;
-    } else {
-        lResult = RegDeleteValueA(hKey, "FFMPEGVM_PATH");
-        if(lResult == ERROR_SUCCESS)
-        {
-            SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 
-                       (LPARAM)"Environment", SMTO_ABORTIFHUNG, 5000, NULL);
-        }
-
-        size_t pos = currentPath.find(dir_str);
-        if (pos == std::string::npos) {
-            std::cout << "ffmpeg-vm not found in PATH" << std::endl;
-            RegCloseKey(hKey);
-            return 0;
-        }
-        newPath = currentPath;
-        newPath.erase(pos, dir_str.length() + 1); // +1 to remove the semicolon
-    }
-
-    // Set new PATH value
-    lResult = RegSetValueExA(hKey, "Path", 0, REG_EXPAND_SZ, 
-                            (const BYTE*)newPath.c_str(), newPath.length() + 1);
-    if (lResult != ERROR_SUCCESS) {
-        std::cerr << "Error: Could not set PATH value" << std::endl;
-        RegCloseKey(hKey);
-        return 1;
     }
 
     RegCloseKey(hKey);
+    return result;
+}
 
-    // Notify system about environment change
-    SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0, 
+static LONG set_registry_key(HKEY root_key, const std::string& sub_key, const std::string& value_name, const std::string& value)
+{
+    HKEY hKey;
+    LONG lResult = RegOpenKeyExA(root_key, sub_key.c_str(), 0, KEY_WRITE, &hKey);
+    if (lResult != ERROR_SUCCESS) return lResult;
+
+    lResult = RegSetValueExA(hKey, value_name.c_str(), 0, REG_EXPAND_SZ,
+                             (const BYTE*)value.c_str(), value.length() + 1);
+
+    RegCloseKey(hKey);
+    return lResult;
+}
+
+static LONG delete_registry_key(HKEY root_key, const std::string& sub_key, const std::string& value_name)
+{
+    HKEY hKey;
+    LONG lResult = RegOpenKeyExA(root_key, sub_key.c_str(), 0, KEY_WRITE, &hKey);
+    if (lResult != ERROR_SUCCESS) return lResult;
+
+    lResult = RegDeleteValueA(hKey, value_name.c_str());
+
+    RegCloseKey(hKey);
+    return lResult;
+}
+
+static void notify_environment_change()
+{
+    SendMessageTimeoutA(HWND_BROADCAST, WM_SETTINGCHANGE, 0,
                        (LPARAM)"Environment", SMTO_ABORTIFHUNG, 5000, NULL);
+}
 
+int set_windows_path(const fs::path& ffmpeg_vm_dir)
+{
+    std::string dir_str = (ffmpeg_vm_dir / "bin").string();
+    std::string currentPath = get_registry_value(HKEY_CURRENT_USER, "Environment", "Path");
+
+    LONG lResult = set_registry_key(HKEY_CURRENT_USER, "Environment", "FFMPEGVM_PATH", ffmpeg_vm_dir.string());
+    if (lResult == ERROR_SUCCESS) {
+        notify_environment_change();
+    }
+
+    if (currentPath.find(dir_str) != std::string::npos) {
+        std::cout << "ffmpeg-vm already in PATH" << std::endl;
+        return 0;
+    }
+
+    std::string newPath = dir_str + ";" + currentPath;
+    lResult = set_registry_key(HKEY_CURRENT_USER, "Environment", "Path", newPath);
+    if (lResult != ERROR_SUCCESS) {
+        std::cerr << "Error: Could not set PATH value" << std::endl;
+        return 1;
+    }
+
+    notify_environment_change();
     return 0;
 }
 
-#endif
+int delete_windows_path(const fs::path& ffmpeg_vm_dir)
+{
+    std::string dir_str = (ffmpeg_vm_dir / "bin").string();
+    std::string currentPath = get_registry_value(HKEY_CURRENT_USER, "Environment", "Path");
 
-#ifdef _WIN32
+    LONG lResult = delete_registry_key(HKEY_CURRENT_USER, "Environment", "FFMPEGVM_PATH");
+    if (lResult == ERROR_SUCCESS) {
+        notify_environment_change();
+    }
+
+    size_t pos = currentPath.find(dir_str);
+    if (pos == std::string::npos) {
+        std::cout << "ffmpeg-vm not found in PATH" << std::endl;
+        return 0;
+    }
+
+    std::string newPath = currentPath;
+    newPath.erase(pos, dir_str.length() + 1);
+
+    lResult = set_registry_key(HKEY_CURRENT_USER, "Environment", "Path", newPath);
+    if (lResult != ERROR_SUCCESS) {
+        std::cerr << "Error: Could not set PATH value" << std::endl;
+        return 1;
+    }
+
+    notify_environment_change();
+    return 0;
+}
 
 int os_setup_env(std::string version, std::filesystem::path ffmpeg_vm_dir, const char* home)
 {
-    int status = update_windows_path(ffmpeg_vm_dir, true);
+    int status = set_windows_path(ffmpeg_vm_dir);
 
     std::ofstream ffmpeg_version(ffmpeg_vm_dir / "VERSION");
 
@@ -117,7 +142,7 @@ int os_setup_env(std::string version, std::filesystem::path ffmpeg_vm_dir, const
 
 int os_remove_env(std::filesystem::path ffmpeg_vm_dir, const char* home)
 {
-    return update_windows_path(ffmpeg_vm_dir, false);
+    return delete_windows_path(ffmpeg_vm_dir);
 }
 
 #endif
