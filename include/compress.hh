b3ec96b747653e7a41b857eb9ed5e7fee0b9f8b3
adriabama06 <adriabama06@gmail.com>
2026-05-29 14:43:09 +0000

    Fix compile error

--- include/compress.hh
+++ include/compress.hh
@@ -5,7 +5,7 @@
 #define COMPRESS_H
 
 int extract_from_memory_to_folder(const std::string &filedata, const std::filesystem::path &destination_dir, void* callback_data, void (*callback)(void*, size_t, size_t));
-int extract_from_memory_to_folder(const std::string &filedata, const std::filesystem::path &destination_dir)
+inline int extract_from_memory_to_folder(const std::string &filedata, const std::filesystem::path &destination_dir)
 {
     return extract_from_memory_to_folder(filedata, destination_dir, NULL, NULL);
 }
