6b659b6c55d9cd5325a448768c3f420f6214859a
adriabama06 <adriabama06@gmail.com>
2026-07-02 00:20:02 +0000

    Change build script

---
+++ build_windows_arm64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Configure with CMake - no cross-compile toolchain needed (native Windows ARM64 build)
+cmake -B build-windows \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_CURL_EXE=OFF \
+  -DCURL_USE_OPENSSL=OFF \
+  -DCURL_USE_SCHANNEL=ON \
+  -DENABLE_TEST=OFF \
+  -DENABLE_INSTALL=OFF \
+  -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" \
+  -DCMAKE_CXX_FLAGS="-static -static-libgcc -static-libstdc++" \
+  -DZLIB_ROOT="" \
+  -DZLIB_INCLUDE_DIR="" \
+  -DZLIB_LIBRARY=""
+
+JOBS=$(nproc)
+
+# Build with limited threads to avoid memory issues
+cmake --build build-windows -j $((JOBS>8 ? 8 : JOBS))
+
+cp build-windows/ffmpeg-version-manager.exe .
