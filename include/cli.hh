ae612e5d8580c78eb56152da1190dac093bdef3d
adriabama06 <adriabama06@gmail.com>
2026-08-10 15:43:13 +0000

    Print CLI error code + error string

--- include/cli.hh
+++ include/cli.hh
@@ -1,6 +1,8 @@
 #ifndef CLI_H
 #define CLI_H
 
+#include "string"
+
 enum CLI_EXIT_STATUS {
     OK = 0,
     UNKNONW_OPTION,
@@ -10,6 +12,19 @@ enum CLI_EXIT_STATUS {
     ERROR_EXTRACTING,
     ALREADY_INSTALLED
 };
+
+inline std::string to_string(CLI_EXIT_STATUS status) {
+    switch (status) {
+        case CLI_EXIT_STATUS::OK: return "OK";
+        case CLI_EXIT_STATUS::UNKNONW_OPTION: return "UNKNONW_OPTION";
+        case CLI_EXIT_STATUS::NO_ARGUMENT: return "NO_ARGUMENT";
+        case CLI_EXIT_STATUS::NO_VERSIONS_TO_INSTALL: return "NO_VERSIONS_TO_INSTALL";
+        case CLI_EXIT_STATUS::ERROR_DOWNLOADING: return "ERROR_DOWNLOADING";
+        case CLI_EXIT_STATUS::ERROR_EXTRACTING: return "ERROR_EXTRACTING";
+        case CLI_EXIT_STATUS::ALREADY_INSTALLED: return "ALREADY_INSTALLED";
+    }
+    return "Unknown";
+}
 
 CLI_EXIT_STATUS run_cli(int argc, char *argv[]);
 
--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,19 @@
 #include "cli.hh"
 #include "tui.hh"
 
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
     if(argc > 1)
     {
-        return run_cli(argc, argv);
+        CLI_EXIT_STATUS status = run_cli(argc, argv);
+
+        if(status != CLI_EXIT_STATUS::OK) {
+            std::cout << "Code exited with error: " << status << " (" << to_string(status) << ")" << std::endl;
+        }
+
+        return status;
     }
 
     return run_tui();
